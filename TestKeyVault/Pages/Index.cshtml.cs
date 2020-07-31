2db6347d9a0daa06379cbf73fa3b420349244d70
pinakee.patnaik <pinakee.patnaik@yahoo.com>
2020-07-31 00:51:08 -0400

    Initial Commit

---
+++ TestKeyVault/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TestKeyVault.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [ViewData]
+        public string ConnectionString { get; set; }
+
+        [ViewData]
+        public string TestToken { get; set; }
+
+        private readonly IConfiguration _config;
+
+        public IndexModel(IConfiguration config)
+        {
+            _config = config;
+            ConnectionString = _config.GetValue(typeof(string), "connectionString").ToString();
+            TestToken = _config.GetValue(typeof(string), "testToken").ToString();
+        }
+
+       public void OnGet()
+        {
+
+        }
+    }
+}
